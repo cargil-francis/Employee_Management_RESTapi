59255801f9c2b89ac2c3e9e50259e900a8746087
cargil-francis <kitty.f9876@gmail.com>
2023-02-12 03:00:11 +0530

    add jwt authentication

--- employee_management_REST/settings.py
+++ employee_management_REST/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ems',
     'rest_framework',
+    'rest_framework_simplejwt',
     
 
 ]
@@ -126,3 +127,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
